09787798802a3984554e99e9632a4bdee1fee810
demianmoschin <demianmoschin@gmail.com>
2015-05-08 09:20:12 -0300

    Finalizada la primer entrega. Pasamos a testing

--- MoverVideos/BL.vb
+++ MoverVideos/BL.vb
@@ -2,9 +2,9 @@ Imports System.IO
 
 Public Class BL
 #Region "VARIABLES DE INSTANCIA"
-    Dim fechaDesde As Date = Today.AddDays(35)
-    Dim directorioOrigen As New IO.DirectoryInfo("d:\videos endoscopio\" & Now.Year.ToString())
-    Dim directorioBackUp As New DirectoryInfo("d:\backup\" & Now.Year.ToString() & "-" & Now.Month.ToString() & "-" & Now.Day.ToString() & "\")
+    Dim fechaDesde As Date = Today.AddDays(-30)
+    Dim directorioOrigen As New IO.DirectoryInfo("D:\VIDEOS ENUTV\" & Now.Year.ToString())
+    Dim directorioBackUp As New DirectoryInfo("D:\BACKUP VIDEOS\" & Now.Year.ToString() & "-" & Now.Month.ToString() & "-" & Now.Day.ToString() & "\")
 #End Region
 #Region "METODOS PUBLICOS"
     Public Sub moverCarpetas()
@@ -21,19 +21,24 @@ Public Class BL
         End If
     End Sub
     Private Sub copiarArchivos()
-        Dim subDirectorio As DirectoryInfo
+
         Try
-            For Each subDirectorio In directorioOrigen.GetDirectories()
-                If subDirectorio.CreationTime.Date < fechaDesde Then
-                    subDirectorio.Refresh()
-                    Dim subDirectorioDestino As New DirectoryInfo(directorioBackUp.ToString() & subDirectorio.ToString())
-                    My.Computer.FileSystem.MoveDirectory(subDirectorio.FullName, subDirectorioDestino.FullName, True)
-                End If
+            For Each directorioMes As DirectoryInfo In directorioOrigen.GetDirectories()
+                directorioMes.Refresh()
+                For Each directorioDia As DirectoryInfo In directorioMes.GetDirectories()
+                    If directorioDia.CreationTime.Date <= fechaDesde Then
+                        'esta linea de prueba la dejo para futuras pruebas, ya que nos indica el formato de salida de una estructura de directorio.
+                        'en un futuro, optar por configurar la estructura desde archivo configurable
+
+                        'My.Computer.FileSystem.WriteAllText("e:\prueba.txt", directorioDia.FullName & vbCrLf & vbCrLf, True)
+                        My.Computer.FileSystem.CopyDirectory(directorioDia.FullName, directorioBackUp.FullName & "\" & directorioMes.Name & directorioDia.Name, True)
+                    End If
+                Next
             Next
+
         Catch ex As Exception
+            My.Computer.FileSystem.WriteAllText("c:\errorlogFileMover.txt", vbCrLf & " fecha =  " & Date.Today.ToString() & vbCrLf & ex.Message & vbCrLf, True)
             Exit Sub
-        Finally
-            subDirectorio = Nothing
         End Try
     End Sub
 
